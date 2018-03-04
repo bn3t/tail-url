3c19b803be3f27182aa911f66fb7d8c387c22b8a
Bernard Niset <bernard.niset@gmail.com>
2018-03-04 22:48:41 +0100

    Changed build.rs to get git commit from env if git is absent.

--- build.rs
+++ build.rs
@@ -4,13 +4,13 @@ use chrono::Local;
 use std::process::Command;
 
 fn main() {
-    // note: add error checking yourself.
+    let unknown: Result<String, &str> = Ok("unknown".to_string());
     let output = Command::new("git")
         .args(&["rev-parse", "--short", "HEAD"])
         .output();
     let git_hash = match output {
         Ok(output) => String::from_utf8(output.stdout).unwrap(),
-        Err(_) => std::env::var("TRAVIS_COMMIT").unwrap(),
+        Err(_) => std::env::var("TRAVIS_COMMIT").or(unknown).unwrap(),
     };
 
     println!("cargo:rustc-env=GIT_COMMIT={}", git_hash);
